d407ee365d11af2e756c20eb86757605e4e64225
varl0x <148959415+v0rl0x@users.noreply.github.com>
2023-12-18 17:28:58 +0900

    Memory leakage fixed

--- banner.go
+++ banner.go
@@ -1,34 +1,43 @@
 package main
 
 import (
-	"bufio"
-	"crypto/tls"
-	"flag"
-	"fmt"
-	"golang.org/x/net/html"
-	"net/http"
-	"os"
-	"strings"
-	"sync"
-	"time"
+    "bufio"
+    "crypto/tls"
+    "flag"
+    "fmt"
+    "golang.org/x/net/html"
+    "net/http"
+    "os"
+    "strings"
+    "sync"
+    "time"
 )
 
 var (
-	bannerString string
-	titleString  string
-	urlPath      string
-	port         string
-	output       string
-	threads      int
+    bannerString string
+    titleString  string
+    urlPath      string
+    port         string
+    output       string
+    threads      int
+    client       *http.Client
 )
 
 func init() {
-	flag.StringVar(&bannerString, "b", "", "string to search for within headers")
-	flag.StringVar(&titleString, "title", "", "string to search for within the title")
-	flag.StringVar(&urlPath, "url", "", "URL path to append to the IP")
-	flag.StringVar(&port, "p", "80", "port number")
-	flag.StringVar(&output, "o", "output.txt", "output file")
-	flag.IntVar(&threads, "t", 1, "number of threads")
+    flag.StringVar(&bannerString, "b", "", "string to search for within headers")
+    flag.StringVar(&titleString, "title", "", "string to search for within the title")
+    flag.StringVar(&urlPath, "url", "", "URL path to append to the IP")
+    flag.StringVar(&port, "p", "80", "port number")
+    flag.StringVar(&output, "o", "output.txt", "output file")
+    flag.IntVar(&threads, "t", 1, "number of threads")
+
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        MaxIdleConns:        100,
+        IdleConnTimeout:     30 * time.Second,
+        DisableCompression:  true,
+    }
+    client = &http.Client{Transport: tr, Timeout: time.Second * 10}
 }
 
 func getHeaders(ip string, port string, urlPath string) (*http.Response, string, string, string, error) {
@@ -45,10 +54,6 @@ func getHeaders(ip string, port string, urlPath string) (*http.Response, string,
 }
 
 func fetchHeaders(url string) (*http.Response, string, string, error) {
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    client := &http.Client{Transport: tr, Timeout: time.Second * 10}
     resp, err := client.Get(url)
     if err != nil {
         return nil, "", "", err
@@ -84,55 +89,57 @@ func fetchHeaders(url string) (*http.Response, string, string, error) {
 }
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	var wg sync.WaitGroup
-	sem := make(chan bool, threads)
+    var wg sync.WaitGroup
+    sem := make(chan bool, threads)
 
-	file, err := os.Create(output)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer file.Close()
+    file, err := os.Create(output)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer file.Close()
+    writer := bufio.NewWriter(file)
+    defer writer.Flush()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		ip := scanner.Text()
-		sem <- true
-		wg.Add(1)
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        ip := scanner.Text()
+        sem <- true
+        wg.Add(1)
 
-go func(ip string) {
-    defer func() {
-        <-sem
-        wg.Done()
-        }()
+        go func(ip string) {
+            defer func() {
+                <-sem
+                wg.Done()
+            }()
 
-        resp, url, headers, title, err := getHeaders(ip, port, urlPath)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Error fetching headers: %v\n", err)
-            return
-        }
+            resp, url, headers, title, err := getHeaders(ip, port, urlPath)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "Error fetching headers: %v\n", err)
+                return
+            }
 
-        if resp != nil && resp.StatusCode == http.StatusOK {
-            pageNotFound := contains(title, "Not Found") || contains(headers, "Not Found") || contains(title, "404") || contains(headers, "404")
+            if resp != nil && resp.StatusCode == http.StatusOK {
+                pageNotFound := contains(title, "Not Found") || contains(headers, "Not Found") || contains(title, "404") || contains(headers, "404")
 
-            if !pageNotFound {
-                matchBanner := bannerString == "" || contains(headers, bannerString)
-                matchTitle := titleString == "" || contains(title, titleString)
+                if !pageNotFound {
+                    matchBanner := bannerString == "" || contains(headers, bannerString)
+                    matchTitle := titleString == "" || contains(title, titleString)
 
-                if (matchBanner && matchTitle) || (bannerString == "" && titleString == "") {
-                    outputString := fmt.Sprintf("%s:%s%s, %s\n%s\nTitle: %s\n\n", ip, port, urlPath, url, headers, title)
-                    fmt.Fprintf(file, outputString)
+                    if (matchBanner && matchTitle) || (bannerString == "" && titleString == "") {
+                        outputString := fmt.Sprintf("%s:%s%s, %s\n%s\nTitle: %s\n\n", ip, port, urlPath, url, headers, title)
+                        fmt.Fprintf(writer, outputString)
+                    }
                 }
             }
-        }
-    }(ip)
-	}
+        }(ip)
+    }
 
-	wg.Wait()
+    wg.Wait()
 }
 
 func contains(s, substr string) bool {
-	return strings.Contains(s, substr)
+    return strings.Contains(s, substr)
 }
